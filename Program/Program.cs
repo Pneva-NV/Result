dcfd3be77716b8c8c9d0741ba2e27ca650ea29ac
Natalia-Pneva <pneva_nv@mail.ru>
2023-01-09 18:32:51 +0300

    filling the array

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Array;
+int count;
+
+Console.Write("Введите количество элементов массива: ");
+count = Convert.ToInt32(Console.ReadLine());
+
+Array = new string[count];
+
+Console.WriteLine("Введите элементы (строки), входящие в массив:");
+for (int i = 0; i < Array.Length; i++)
+{
+    Console.Write("Элемент[{0}] - ", i);
+    Array[i] = Console.ReadLine();
+}
